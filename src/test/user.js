1a59a1f913d186bca8898729b93f64d31c28054a
MahletH <mahletdereje23@gmail.com>
2022-04-15 08:18:34 +0300

    test user creation and session apis

--- src/test/user.js
+++ src/test/user.js
@@ -6,6 +6,17 @@ const { expect } = chai;
 use(chaiHttp);
 
 describe("User API",() =>{
+    let tokens;
+    let userTokens;
+    before(async()=>{
+        let response = await chai.request(app).post("/api/login").send(
+            {
+                "emailOrUsername": "admin",
+                "password": "itsc2022"
+            });
+        tokens = (response.body.accessToken);
+    });
+    
     it("It should authenticate user", async () => {
         let response = await chai.request(app).post("/api/login").send(
             {
@@ -18,4 +29,56 @@ describe("User API",() =>{
         expect(response.body).to.have.property("msg").eql('Login Successful.');
      
       });
+    
+      it("It should register user", async () => {
+        let response = await chai
+          .request(app)
+          .post("/api/signup")
+          .send({
+            username: "mahi",
+            password: "UnitTesting",
+            email: "user@test.com",
+            role: "propertyAdminUser",
+          }).set('Authorization', 'JWT ' + tokens);
+        expect(response).to.have.status(201);
+        expect(response.body).to.be.a("object");
+        expect(response.body).to.have.property("msg").eql('User registered successfully');
+      });
+    it("It should logout user", async () => {
+        let response = await chai
+          .request(app)
+          .post('/api/user/logout')
+          .send({
+            emailOrUsername: "mahi",
+            password: "123456",
+          });
+        //   console.log(response.body);
+        expect(response).to.have.status(200);
+        expect(response.body).to.be.a("object");
+        expect(response.body).to.have.property("msg").eql('Logout successful.');
+      });
+    it("It should refresh user's token", async () => {
+        let response = await chai.request(app).post("/api/login").send(
+            {
+                "emailOrUsername": "mahi",
+                "password": "123456"
+            });
+        expect(response).to.have.status(200);
+        expect(response.body).to.be.a("object");
+        expect(response.body).to.have.property("accessToken");
+        expect(response.body).to.have.property("msg").eql('Login Successful.');
+        userTokens = response.header["set-cookie"][0];
+        response = await chai
+          .request(app)
+          .post('/api/user/refreshToken')
+          .send({
+            emailOrUsername: "mahi",
+            password: "123456",
+          }).
+          set('Cookie', userTokens);
+        expect(response).to.have.status(200);
+        expect(response.body).to.be.a("object");
+        expect(response.body).to.have.property("accessToken");
+      });
+
 });

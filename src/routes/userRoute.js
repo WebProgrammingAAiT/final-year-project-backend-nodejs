c600bbe4f59256408bc62aeb9cd9f06c013c18a7
Abdi Mussa <abdimussa87@gmail.com>
2022-04-16 14:42:41 +0300

    automated prettier added before commit

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,14 +1,28 @@
-import express from 'express';
-import userCtrl from '../controllers/userController.js';
-import { hasValidToken,isAdmin } from "../middleware/authMiddleware.js";
-
+import express from "express";
+import userCtrl from "../controllers/userController.js";
+import { hasValidToken, isAdmin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.put('/user/changeDepartment',hasValidToken,isAdmin,userCtrl.changeUserDepartment);
-router.get('/users',hasValidToken,isAdmin,  userCtrl.getUsers);
-router.put('/users/:emailOrUsername/changeRole',hasValidToken,isAdmin, userCtrl.changeUserRole);
+router.put(
+  "/user/changeDepartment",
+  hasValidToken,
+  isAdmin,
+  userCtrl.changeUserDepartment
+);
+router.get("/users", hasValidToken, isAdmin, userCtrl.getUsers);
+router.put(
+  "/users/:emailOrUsername/changeRole",
+  hasValidToken,
+  isAdmin,
+  userCtrl.changeUserRole
+);
 //admin changing another user's password
-router.put('/users/:emailOrUsername/changePassword', hasValidToken,isAdmin, userCtrl.changeUserPassword)
+router.put(
+  "/users/:emailOrUsername/changePassword",
+  hasValidToken,
+  isAdmin,
+  userCtrl.changeUserPassword
+);
 
 export default router;
